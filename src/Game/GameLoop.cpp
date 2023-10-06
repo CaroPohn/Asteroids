6cb75a5d38fbcc6d40935af14d471538b78481d9
CaroPohn <carolinapohn@gmail.com>
2023-10-06 10:40:26 -0300

    Create scenes and game loop

---
+++ src/Game/GameLoop.cpp
@@ -0,0 +1,55 @@
+#include "GameLoop.h"
+#include "header/raylib.h"
+#include "Scenes.h"
+
+static Scenes scene;
+
+void GameLoop()
+{
+	const int screenWidth = 1200;
+	const int screenHeight = 900;
+
+	InitWindow(screenWidth, screenHeight, "Asteroids");
+
+	scene = Scenes::Menu;
+	Scenes previousScene = Scenes::Exit;
+	bool isNewScene;
+
+	do
+	{
+		isNewScene = (scene != previousScene);
+		previousScene = scene;
+
+		switch (scene)
+		{
+		case Scenes::Menu:
+			break;
+
+		case Scenes::Play:
+			break;
+
+		case Scenes::Pause:
+			break;
+
+		case Scenes::Rules:
+			break;
+
+		case Scenes::Credits:
+			break;
+
+		case Scenes::WinScreen:
+			break;
+
+		case Scenes::LoseScreen:
+			break;
+
+		case Scenes::Exit:
+			break;
+
+		default:
+			break;
+		}
+	} while (!WindowShouldClose() && scene != Scenes::Exit);
+
+	CloseWindow();
+}
---
+++ src/Game/Scenes.h
@@ -0,0 +1,13 @@
+#pragma once
+
+enum class Scenes
+{
+    Menu,
+    Play,
+    Pause,
+    Rules,
+    Credits,
+    WinScreen,
+    LoseScreen,
+    Exit
+};
