b91613a9e569981863de83e688e80e007ea75de8
Caro Pohn <carolinapohn@gmail.com>
2023-10-19 13:31:41 -0300

    Add struct button and its functions init, draw, and checkcollision

---
+++ src/Objects/Button.cpp
@@ -0,0 +1,34 @@
+#include "Objects/Button.h"
+
+void InitButton(Button& button, Texture2D texture, float x, float y, float width, float height, Color color)
+{
+	button.texture = texture;
+	button.x = x;
+	button.y = y;
+	button.width = width;
+	button.height = height;
+	button.color = color;
+	button.isSelected = false;
+}
+
+void DrawButton(Button& button)
+{
+	if (button.isSelected)
+	{
+		button.color = GRAY;
+	}
+	else
+		button.color = RAYWHITE;
+
+	DrawTexture(button.texture, button.x, button.y, button.color);
+}
+
+bool CheckCollisionButtonMouse(Vector2 mousePos, Button button)
+{
+	return ((mousePos.x >= button.x) && (mousePos.x < (button.x + button.width)) && (mousePos.y >= button.y) && (mousePos.y < (button.y + button.height)));
+}
+
+bool CheckMouseInput(Button button)
+{
+	return (CheckCollisionButtonMouse(GetMousePosition(), button) && IsMouseButtonReleased(MOUSE_BUTTON_LEFT));
+}
---
+++ src/Objects/Button.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "header/raylib.h"
+
+struct Button
+{
+	Texture2D texture;
+	float x;
+	float y;
+	float width;
+	float height;
+	Color color;
+	bool isSelected;
+};
+
+void InitButton(Button& button, Texture2D texture, float x, float y, float width, float height, Color color);
+void DrawButton(Button& button);
+bool CheckCollisionButtonMouse(Vector2 mousePos, Button button);
+bool CheckMouseInput(Button button);
