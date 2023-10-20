c0a3bd87c6ae249dd285ffbcef6ab308a9a15675
Caro Pohn <carolinapohn@gmail.com>
2023-10-20 01:10:41 -0300

    Add declarations in menu.h

--- src/Game/Menu.h
+++ src/Game/Menu.h
@@ -1,2 +1,9 @@
 #pragma once
+#include "Objects/Button.h"
+#include "Game/Scenes.h"
 
+void InitMenu();
+void InitMenuButtons(Button& playButton, Button& rulesButton, Button& creditsButton, Button& exitButton);
+void DrawMenu();
+void MenuInput(Button& playButton, Button& rulesButton, Button& creditsButton, Button& exitButton, Scenes& scene);
+void RunMenu(Scenes& scene, bool isNewScene);
