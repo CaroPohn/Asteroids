65bfc766ba8436d6973d09b50f122856bde39b2b
CaroPohn <carolinapohn@gmail.com>
2023-10-25 12:43:41 -0300

    Add play update and draw functions

--- src/Game/Play.cpp
+++ src/Game/Play.cpp
@@ -2,5 +2,28 @@
 
 namespace Asteroids
 {
+	static const int MAX_ASTEROIDS = 64;
 
+	Asteroid static asteroidsArr[MAX_ASTEROIDS] = { 0 };
+
+	void Update()
+	{
+		for (int i = 0; i < MAX_ASTEROIDS; i++)
+		{
+			AsteroidUpdate(asteroidsArr[i]);
+		}
+	}
+
+	void Drawing()
+	{
+		BeginDrawing();
+
+		ClearBackground(BLACK);
+		for (int i = 0; i < MAX_ASTEROIDS; i++)
+		{
+			AsteroidDraw(asteroidsArr[i]);
+		}
+
+		EndDrawing();
+	}
 }
